f072f57986cdc079b649b4554d1ac96f21d39a3a
bisma-r <rauf.bisma53@gmail.com>
2025-07-16 00:08:27 +0500

    normalization function added

--- Multiple_linear_regression.py
+++ Multiple_linear_regression.py
@@ -4,55 +4,75 @@ import numpy as np
 data = fetch_california_housing(as_frame=True)
 df = data.frame
 
-x = np.array(df.drop(columns=['MedHouseVal']).values.tolist())
-y = np.array(df['MedHouseVal'].tolist())
+x = np.array(df.drop(columns=['MedHouseVal']).values)
+y = np.array(df['MedHouseVal'].values)
 
 class MLR:
-    def __init__(self, w, m = len(x),  b = 1, a = 0.01, d = 0, j = 2):
-        self.w = w
-        self.b = b
-        self.m = m
-        self.d = d
+    def __init__(self, x, y, a=0.01):
+        self.x = x
+        self.y = y
+        self.m, self.n = x.shape  # m = number of samples, n = number of features
+        self.w = np.ones(self.n)
+        self.b = 1
         self.a = a
-        self.j = j
+        self.j = float('inf')  # initial cost set as infinity
 
     def cost_function(self):
-        self.j = (1 / (2 * self.m)) * self.sum_function()
-        return self.j
+        predictions = np.dot(self.x, self.w) + self.b
+        errors = predictions - self.y
+        cost = (1 / (2 * self.m)) * np.sum(errors ** 2)
+        return cost
 
-    def sum_function(self, e = 0, b1 = 0):
-        s = 0
-        c = 0
-        self.w = np.array(self.w)
-        if self.d == 0:
-            for i in x:
-                s += ((np.dot(self.w, i) + self.b - y[c]) ** 2)
-                c = c + 1
-        elif self.d == 1:
-            for i in x:
-                s += ((np.dot(self.w, i) + b1 - y[c]) * i[e])
-                c = c + 1
-        else:
-            for i in x:
-                s += (np.dot(self.w, i) + self.b - y[c])
-                c = c + 1
-        return s
+    def update_parameters(self):
+        predictions = np.dot(self.x, self.w) + self.b
+        errors = predictions - self.y
 
-    def update_para(self):
-        temp = self.b
-        self.d = 2
-        self.b = self.b - (self.a * ((1 / self.m) * self.sum_function()))
-        self.d = 1
-        for index, i in enumerate(self.w):
-            i = i - (self.a * ((1 / self.m) * self.sum_function(index, temp)))
-        return self.w, self.b
+        dw = (1 / self.m) * np.dot(self.x.T, errors)
+        db = (1 / self.m) * np.sum(errors)
+
+        self.w -= self.a * dw
+        self.b -= self.a * db
+
+    def run(self, max_iter=10000, tolerance=1e-6):
+        self.normalize()
+        count = 0
+        while self.j > tolerance and count < max_iter:
+            self.j = self.cost_function()
+            self.update_parameters()
+            count += 1
+            #if count % 100 == 0 or count == 1:
+            print(f"Iter {count}: cost = {self.j:.6f}, b = {self.b:.4f}, w[0] = {self.w[0]:.4f}")
+        print(f"Training completed in {count} iterations.")
+
+    def normalize(self):
+        s = [0] * self.n
+        s1 = [0] * self.n
+        mean = [0] * self.n
+        sd = [0] * self.n
+
+        for row in self.x:
+            for i, val in enumerate(row):
+                s[i] += val
+
+        # Compute mean for each feature
+        for i in range(self.n):
+            mean[i] = s[i] / self.m
+
+        # Compute standard deviation for each feature
+        for row in self.x:
+            for i, val in enumerate(row):
+                s1[i] += (val - mean[i]) ** 2
+
+        for i in range(self.n):
+            sd[i] = (s1[i] / self.m) ** 0.5
+
+        for row in self.x:
+            for i in range(self.n):
+                if sd[i] != 0:
+                    row[i] = (row[i] - mean[i]) / sd[i]
+                else:
+                    row[i] = 0
 
 
-# max_iter = 10000 #chatGPT recommendation
-# count = 0
-# while j > 1e-6 and count < max_iter:
-#
-#     j = cost_function(w, b, m)
-#     w, b = update_para(w, b, a, m)
-#     count +=1
-#     print ("w = ", w, " b = ", b, " j = ", j )
+mlr = MLR(x, y)
+mlr.run()
