26e584209373ee3e353e69903934cacc31c97e3a
bisma-r <rauf.bisma53@gmail.com>
2025-08-24 17:22:39 +0500

    added small application of the model (predicting salary based on years of experience)

--- Linear_regression.py
+++ Linear_regression.py
@@ -1,5 +1,5 @@
-def cost_function(w1, b1, m1):
-    j1 = (1 / (2 * m1)) * sum_function(w1, b1, xy, d)
+def cost_function(w1, b1, m1, xy2):
+    j1 = (1 / (2 * m1)) * sum_function(w1, b1, xy2, 0)
     return j1
 
 def sum_function(w2, b2, xy1: list, d1):
@@ -15,27 +15,55 @@ def sum_function(w2, b2, xy1: list, d1):
             s += (w2 * i[0] + b2 - i[1])
     return s
 
-def update_para(w3, b3, a1, m2):
-    temp = w3
+def update_para(w3, b3, a1, m2, xy3):
+    old_w, old_b = w3, b3
     d2 = 1
-    w3 = w3 - (a1 * ((1 / m2) * sum_function(w3, b3, xy, d2)))
+    w3 = w3 - (a1 * ((1 / m2) * sum_function(old_w, old_b, xy3, d2)))
     d2 = 2
-    b3 = b3 - (a1 * ((1/m2) * sum_function(temp, b3, xy, d2)))
+    b3 = b3 - (a1 * ((1/m2) * sum_function(old_w, old_b, xy3, d2)))
     return w3, b3
 
 xy = [[1, 3], [2, 5], [3, 7], [4, 9], [5, 11]]
-w = 1
-b = 1
-m = len(xy)
-a = 0.01
-d = 0
-j = 2
-max_iter = 10000 #chatGPT recommendation
-count = 0
 
-while j > 1e-6 and count < max_iter:
-    prevJ = j
-    j = cost_function(w, b, m)
-    w, b = update_para(w, b, a, m)
-    count +=1
-    print ("w = ", w, " b = ", b, " j = ", j )
+def run(l):
+    w = 1
+    b = 1
+    m = len(l)
+    a = 0.01
+    j = 2
+    max_iter = 10000 #chatGPT recommendation
+    count = 0
+    prev_j = float("inf")
+
+    while abs(prev_j - j) > 1 and count < max_iter:
+        prev_j = j
+        j = cost_function(w, b, m, l)
+        w, b = update_para(w, b, a, m, l)
+        count +=1
+        print ("w = ", w, " b = ", b, " j = ", j )
+    print("No. of iterations: ", count)
+    return w, b
+
+#Predict salaries based on years of experience.
+
+# Small dataset
+data = {
+    'YearsExperience': [1.1, 1.3, 1.5, 2.0, 2.2, 2.9, 3.0, 3.2, 3.2, 3.7,
+                        3.9, 4.0, 4.0, 4.1, 4.5, 4.9, 5.1, 5.3, 5.9, 6.0,
+                        6.8, 7.1, 7.9, 8.2, 8.7, 9.0, 9.5, 9.6, 10.3, 10.5],
+    'Salary': [39343, 46205, 37731, 43525, 39891, 56642, 60150, 54445, 64445, 57189,
+               63218, 55794, 56957, 57081, 61111, 67938, 66029, 83088, 81363, 93940,
+               91738, 98273, 101302, 113812, 109431, 105582, 116969, 112635, 122391, 121872]
+}
+
+data_list = [[x, y] for x, y in zip(data['YearsExperience'], data['Salary'])]
+
+w, b = run(data_list)
+
+def predict(val, w4, b4):
+    ans = (val * w4) + b4
+    print(ans)
+
+predict(5.1, w, b)
+data_dict = {x: y for x, y in data_list}
+print(data_dict[5.1])
